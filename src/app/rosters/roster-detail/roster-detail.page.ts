207bb3c758a362990e9bed5fc9bbd139978f3afc
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2019-08-11 18:12:56 +0100

    added delete functionality on roster

--- src/app/rosters/roster-detail/roster-detail.page.ts
+++ src/app/rosters/roster-detail/roster-detail.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RosterService } from '../roster.service';
 import { Roster } from '../roster.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-roster-detail',
@@ -13,7 +14,9 @@ export class RosterDetailPage implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private rosterService: RosterService) { }
+    private rosterService: RosterService,
+    private alertController: AlertController,
+    private router: Router) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -27,4 +30,22 @@ export class RosterDetailPage implements OnInit {
     });
   }
 
+  onDeleteRoster() {
+    this.alertController.create({
+      header: 'Are you sure?',
+      message: 'Are you sure you want to delete this roster?',
+      buttons: [{
+        text: 'Cancel',
+        role: 'Canel'
+      },
+    {
+      text: 'Delete',
+      handler: () => {
+        this.rosterService.deleteRoster(this.roster.id);
+        this.router.navigate(['/rosters']);
+      }
+    }]}).then(alertElement => {
+      alertElement.present();
+    });
+  }
 }
